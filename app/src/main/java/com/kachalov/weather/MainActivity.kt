a5581c58cec7ffc9fe43c22b3328a44738fef3eb
DisterRU <disterko@gmail.com>
2020-01-22 18:02:53 +0300

    current state

--- app/src/main/java/com/kachalov/weather/MainActivity.kt
+++ app/src/main/java/com/kachalov/weather/MainActivity.kt
@@ -1,24 +1,23 @@
 package com.kachalov.weather
 
 import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
-import androidx.appcompat.app.AppCompatActivity
+import com.kachalov.weather.constants.Codes
 import com.kachalov.weather.constants.Fragments
 import com.kachalov.weather.constants.Preferences
 import com.kachalov.weather.observers.CitiesObserver
 import com.kachalov.weather.utils.FragmentChanger
 import com.kachalov.weather.utils.FragmentFinder
-import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity(), FragmentChanger {
+class MainActivity : BaseActivity(), FragmentChanger {
     private lateinit var fragmentFinder: FragmentFinder
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(toolbar)
         initFragmentFinder()
         changeFragment(Fragments.CITIES)
     }
@@ -37,8 +36,24 @@ class MainActivity : AppCompatActivity(), FragmentChanger {
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
-            R.id.action_settings -> true
+            R.id.action_settings -> {
+                val intent = Intent(this@MainActivity, SettingsActivity::class.java)
+                startActivityForResult(intent, Codes.SETTINGS_CODE)
+                true
+            }
+            R.id.action_clear -> {
+                clearCities()
+                recreate()
+                true
+            }
             else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == Codes.SETTINGS_CODE) {
+            recreate()
         }
     }
 
--- app/src/main/java/com/kachalov/weather/constants/Preferences.kt
+++ app/src/main/java/com/kachalov/weather/constants/Preferences.kt
@@ -2,6 +2,6 @@ package com.kachalov.weather.constants
 
 object Preferences {
     const val CITIES = "cities_pref"
-    const val COLORS = "colors_pref"
+    const val THEMES = "themes_pref"
 }
 
